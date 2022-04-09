6afe5239bf53c97910e70ebcd3c9b025fd235399
Daniel Card <dan.card@xservus.com>
2022-04-09 06:21:21 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,24 @@
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install docker-compose -y
+
+docker volume create statping-persistent-storage
+docker volume ls
+docker volume inspect statping-persistent-storage
+
+
+#This will run docker headless but persistent
+
+docker run -d -it -p 8080:8080 --mount source=statping-persistent-storage,target=/statping --name persistent-statping statping/statping
+
+#Setup
+http://x.x.x.x:8080/
+#Dashboard
+http://x.x.x.x:8080/login
+
+
+#Stop the container
+docker stop persistent-statping
+
+#start the container
+docker start persistent-statping
